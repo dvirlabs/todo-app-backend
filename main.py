677c8c80472f6da383ffe775f58fa6d29d5fd7e6
dvirlabs <dvirlabs@gmail.com>
2023-02-17 17:06:07 +0200

    fix css

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from db_utils import *
 # init app
 app = FastAPI()
 
-# Mount the static files directory at "/static/css"
+# Mount the static files directory at "/static"
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -30,4 +30,4 @@ async def get_result():
 
 
 if __name__ == '__main__':
-  uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, access_log=False)
+  uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True, access_log=False)
