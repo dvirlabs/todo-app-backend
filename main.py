50f2e00e083143dd0de90e506eb3f692efd9ebc9
dvirlabs <dvirlabs@gmail.com>
2023-02-18 23:04:08 +0200

    change port

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ async def get_result():
 
 
 if __name__ == '__main__':
-  uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True, access_log=False)
+  uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, access_log=False)
