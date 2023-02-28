f9b55506e58e689c3ee35b34ff60589643e6cb8d
dvirlabs <dvirlabs@gmail.com>
2023-02-28 22:21:06 +0200

    undo add button

---
+++ template/static/script.js
@@ -0,0 +1,49 @@
+function onLoad() {
+  var isDarkmode = localStorage.getItem("darkmode") === "true"
+  if (isDarkmode){
+    toggleDarkMode()
+  }
+  $.ajax({
+    type: 'GET',
+    url: '/table',
+    dataType: 'json',
+    success: function(data) {
+      var table = $('#pg_table');
+      $.each(data.results, function(index, row) {
+        table.append(
+          $('<tr>').append(
+            $('<td>').text(row.id),
+            $('<td>').text(row.task),
+            $('<td>').text(row.status)
+          )
+        );
+      });
+    }
+  });
+}
+
+
+
+function toggleDarkMode() {
+  var body = document.getElementsByTagName('body')[0];
+  body.classList.toggle('dark-mode');
+  var isDarkMode = body.classList.contains('dark-mode');
+  localStorage.setItem('darkmode', String(isDarkMode))
+
+}
+
+
+function updateDatabase() {
+  
+}
+
+
+function clearTable() {
+  $.ajax({
+    type: 'delete',
+    url: '/clear_table'
+  })
+  location.reload();
+}
+
+
